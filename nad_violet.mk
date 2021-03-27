a0a67c4e33f3f10a8d733dfa21f72b813293a0d7
jash <jashvakharia@gmail.com>
2021-03-27 07:24:54 +0000

    violet: Add OFFICIAL tag

--- nad_violet.mk
+++ nad_violet.mk
@@ -13,6 +13,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
+NAD_BUILD_TYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := nad_violet
